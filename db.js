0761e52324aa5b0faf1669b95f0ac01dda71d6ea
feline199x <fdbantiveros21@gmail.com>
2021-05-30 20:39:21 +0800

    2nd update

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
+  user: "thotoawqxwfhpj",
+  password: "07e55f074c0d4c53eca778bcc6a58cdfa34182950944ca675bdbc4afcef1e2f4",
+  host: "ec2-54-225-228-142.compute-1.amazonaws.com",
   port: 5432,
-  database: "pern-todo",
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  database: "df9tlk0mo2231u",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
